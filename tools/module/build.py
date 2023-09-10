6171c4ed3bb77001395003ad6147339a7ede96c5
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-10 00:15:18 -0600

    placeholder commands

---
+++ tools/module/build.py
@@ -0,0 +1,4 @@
+from .response import Response
+
+def run(arguments: list) -> Response:
+    return Response("Build", Response.ERROR, "The build command is not yet implemented")
---
+++ tools/module/lock.py
@@ -0,0 +1,4 @@
+from .response import Response
+
+def run(arguments: list) -> Response:
+    return Response("Lock", Response.ERROR, "The lock command is not yet implemented")
