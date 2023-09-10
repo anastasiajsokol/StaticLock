78ef348c7e5617733e9a10ba74eb034c9cc0869e
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-10 00:16:12 -0600

    fix signatures of placeholders

--- tools/module/build.py
+++ tools/module/build.py
@@ -1,4 +1,4 @@
 from .response import Response
 
-def run(arguments: list) -> Response:
+def run(arguments: list, _: str) -> Response:
     return Response("Build", Response.ERROR, "The build command is not yet implemented")
--- tools/module/lock.py
+++ tools/module/lock.py
@@ -1,4 +1,4 @@
 from .response import Response
 
-def run(arguments: list) -> Response:
+def run(arguments: list, _: str) -> Response:
     return Response("Lock", Response.ERROR, "The lock command is not yet implemented")
