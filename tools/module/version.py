5af71642b61970fddf384a72b6479d58aca4a3d1
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-10 11:48:53 -0600

    add VERSION variable

--- tools/module/version.py
+++ tools/module/version.py
@@ -1,6 +1,8 @@
 from .response import Response
 
+VERSION = "0.1"
+
 def run(arguments: list, _: str) -> Response:
     if(len(arguments) != 0):
-        return Response("Version", Response.WARNING, "StaticLock Toolchain v0.1 does not accept arguments to version command")
-    return Response("Version", Response.INFO, "StaticLock Toolchain v0.1")
+        return Response("Version", Response.WARNING, f"StaticLock Toolchain v{VERSION} does not accept arguments to version command")
+    return Response("Version", Response.INFO, f"StaticLock Toolchain v{VERSION}")
