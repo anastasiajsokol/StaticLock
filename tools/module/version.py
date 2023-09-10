46d395a0f66f31ce1a0ede986fc6857add798b35
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-09-10 00:14:28 -0600

    placeholder version command

--- tools/module/version.py
+++ tools/module/version.py
@@ -1,6 +1,6 @@
 from .response import Response
 
-def run(arguments: list) -> Response:
+def run(arguments: list, _: str) -> Response:
     if(len(arguments) != 0):
         return Response("Version", Response.WARNING, "StaticLock Toolchain v0.1 does not accept arguments to version command")
     return Response("Version", Response.INFO, "StaticLock Toolchain v0.1")
