4877260b29eb5c1c53c22b8e8a887eb8594517b5
Anastasia <anastasia.j.sokol@gmail.com>
2023-09-11 18:45:59 -0600

    upload progress

--- tools/module/build.py
+++ tools/module/build.py
@@ -1,4 +1,9 @@
 from .response import Response
+from .security.encrypt import encrypt
+
+class Config:
+    def __init__(self, arguments: list):
+
 
 def run(arguments: list, _: str) -> Response:
     return Response("Build", Response.ERROR, "The build command is not yet implemented")
