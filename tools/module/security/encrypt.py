b817245f051c47836917efc2b4dabda78b1c0553
Anastasia <anastasia.j.sokol@gmail.com>
2023-09-12 16:30:42 -0600

    save progress

--- tools/module/security/encrypt.py
+++ tools/module/security/encrypt.py
@@ -1,9 +1,10 @@
-from types import Tuple
-
 from .. import response
 
-def hash(password: bytes, salt: bytes) -> Tuple[bytes, bytes]:
+def hash(password: bytes, salt: bytes) -> tuple[bytes, bytes]:
     return b''
 
 def encrypt_file(source: str, destination: str, key: bytes) -> response.Response:
+    return
+
+def encrypt_directory(source: str, destination: str, key: bytes) -> response.Response:
     return
