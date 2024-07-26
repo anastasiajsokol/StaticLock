f2c79de651fda40f1247aa0545030c92f4976af2
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-07-26 16:15:56 -0600

    implement example AES-GCM

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,25 @@
         lisense     The Unliscense (see repository for more information)
 */
 
+use aes_gcm::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    Aes256Gcm
+};
+
 fn main(){
-    println!("Hello world!");
+    // The encryption key can be generated randomly:
+    let key = Aes256Gcm::generate_key(OsRng);
+
+    let cipher = Aes256Gcm::new(&key);
+    let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+    
+    let ciphertext = cipher.encrypt(&nonce, b"plaintext message".as_ref()).expect("Failed to encrypt");
+    
+    println!("{:?}", ciphertext);
+
+    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).expect("Failed to decrypt");
+
+    println!("{:?}", plaintext);
+
+    assert_eq!(&plaintext, b"plaintext message");
 }
