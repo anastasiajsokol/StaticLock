682456f51004d63f362f41ade8f5b929b38c4d89
Anastasia Sokol <anastasia.j.sokol@gmail.com>
2024-07-27 15:01:44 -0600

    full service worker proof-of-concept (no decryption)

--- frontend/staticlock.js
+++ frontend/staticlock.js
@@ -5,7 +5,38 @@
         lisense     The Unliscense (see repository for more information)
 */
 
+// Personal State //
+
+self.database = {
+    "/frogs.say": true
+};
+
+// Initialization //
+
+skipWaiting()
+
 self.addEventListener("activate", (event) => {
     event.waitUntil(clients.claim());
     console.log("Active");
+});
+
+// Message and Command Handling //
+
+self.addEventListener("message", (event) => {
+    console.log("Service Worker: ", event.data);
+});
+
+// Decryption and Passthrough Handling //
+
+self.addEventListener("fetch", (event) => {
+    const request = event.request;
+    let path = new URL(request.url).pathname;
+
+    console.log("Fetching ", path);
+
+    if(path in self.database) {
+        event.respondWith(new Response("hello!"));
+    } else {
+        event.respondWith(fetch(request));
+    }
 });
